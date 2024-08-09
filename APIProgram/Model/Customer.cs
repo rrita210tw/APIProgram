8d010852c13bca12671580a674d69f3ec415e914
Rita_Huang <rrita210tw@gmail.com.tw>
2024-08-09 20:48:39 +0800

    Add Program

---
+++ APIProgram/Model/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIProgram.Model
+{
+    public class Customer
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int id { get; set; }
+
+        /// <summary>
+        /// 會員卡號
+        /// </summary>
+        public string cardNo { get; set; }
+
+        /// <summary>
+        /// 會員名稱
+        /// </summary>
+        public string CustomerName { get; set; }
+        /// <summary>
+        /// 行動電話
+        /// </summary>
+        public string phone { get; set; }
+        /// <summary>
+        /// 會員生日
+        /// </summary>
+        public string birthdate { get; set; }
+
+    }
+}
